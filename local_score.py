d97976b2ad9506cfc8a5ca52953b687c897786c2
Costas Bouyioukos <cbouyio@gmail.com>
2019-02-05 20:06:46 +0100

    Include error handing and external specification of the scoring function.

--- local_score.py
+++ local_score.py
@@ -3,14 +3,7 @@
 """A python tool to calculate local score of DNA/RNA sequences."""
 
 
-SCORING_DNA = {"A" : -1.0,
-               "C" : 2.0,
-               "G" : 1.0,
-               "T" : -1.0}
-# TODO perhaps have the dictionary also as an external file for example for proteins.
-
-
-def local_score(sequence, scoring = SCORING_DNA):
+def local_score(sequence, scoring):
     """Main recursive function to calculate local score.
 
     sequence: A sequence of symbols (i.e string)
@@ -19,7 +12,10 @@ def local_score(sequence, scoring = SCORING_DNA):
     """
     local_scores = [0.0]  # Initial local score is zero by definition.
     for symbol in sequence:
-        local_scores.append(max(0, local_scores[-1] + scoring[symbol]))
+        try:
+            local_scores.append(max(0, local_scores[-1] + scoring[symbol]))
+        except KeyError:
+            raise KeyError('Symbol {} not found in scoring dictionary. Please check your scoring function and/or your input file.'.format(symbol))
     return(local_scores[1:])
 
 
@@ -32,11 +28,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='localScore', description='Calculate and prints the local score of biosequences.', epilog="Author: Costas Bouyioukos, 2018")
     parser.add_argument('infile', nargs='?', default='-', type=argparse.FileType('r'), metavar="input_file", help='Path to a fasta DNA/RNA/protein file. (or STDIN)')
     parser.add_argument("outfile", nargs='?', default='-', type=argparse.FileType('w'), metavar='output_file', help="Path for the results file. (or STDOUT)")
-    parser.add_argument('-s', '--scoring', type=str, help='A scoring system. Default: a hardcoded SCORING_DNA dictionary.')
+    parser.add_argument('-s', '--scoring', type=str, help='A scoring system file (a whitespace separated file with nucl./aa. first column and score as seconf)). Default: a hardcoded SCORING_DNA dictionary for GC richness.', dest='scr')
     optArgs = parser.parse_args()
 
+    # Choose the scoring functionself.
+    SCORING = {}
+    if optArgs.scr :
+        with open(optArgs.scr) as f:
+            for line in f:
+                (key, val) = line.split()
+                SCORING[key] = float(val)
+    else:
+        # By default the local score finds GC reach regions.
+        SCORING = {"A" : -1.0,
+                   "C" : 1.0,
+                   "G" : 1.0,
+                   "T" : -1.0}
+                   
     # Iterate over all fasta sequences.
     for seq in SeqIO.parse(optArgs.infile, "fasta"):
-        ls = local_score(seq.seq)
-        optArgs.outfile.write("ID: {}\nSeq: {}\nScore: {}".format(seq.id, seq.seq, ' '.join(map(str, ls))))
+        ls = local_score(seq.seq, SCORING)
+        optArgs.outfile.write("ID: {}  LS_max: {}\nSeq: {}\nScore: {}".format(seq.id, max(ls), seq.seq, ' '.join(map(str, ls))))
     optArgs.outfile.write("\n")
